edfb835600c3ac7e622760445595ebe420c3f6b8
luka wandanje <lukawandanje@gmail.com>
2019-03-23 14:00:36 +0300

    add javascript content

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-function triangle(sideA,sideB,sideC) {
-	var sideA (prompt("enter a value for side A"))
 
+function triangle (sideA,sideB,sideC) {
+    var sideA = prompt("Enter a value for side A: ");
+    var sideB = prompt("Enter a value for side B: ");
+    var sideC = prompt("Enter a value for side C: ");
+
+    var a = parseInt(sideA);
+    var b = parseInt(sideB);
+    var c = parseInt(sideC);
+
+    if (a===b && b===c && c===a) {
+        alert("Equilateral");
+    }
+    else if (a===b && a!==c && b!==c || b===c && b!==a && c!==a || a===c && a!==b && b!==c) {
+        alert("isosceles");
+    }
+    else if(a!==b && b!==c && c!==a)  {
+        alert("scalene");
+    }
+    else{
+        alert("Not a Number");
+    }
+}
+triangle();
+
+
+if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+    triType.textContent = "Please enter numbers only!";
+}
+if (sideA===null || sideB===null || sideC===null){
+	alert("Please enter numerical values")
 }

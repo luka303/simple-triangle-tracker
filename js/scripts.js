ab5286e9fe026c2e1e6fd23c717c4c8d875283ab
luka wandanje <lukawandanje@gmail.com>
2019-03-24 18:25:05 +0300

    add input space for figures

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 function triangle (sideA,sideB,sideC) {
     var sideA = prompt("Enter a value for side A: ");
     var sideB = prompt("Enter a value for side B: ");
-    var sideC = prompt("Enter a value for side A: ");
+    var sideC = prompt("Enter a value for side C: ");
 
     var a = parseInt(sideA);
     var b = parseInt(sideB);
     var c = parseInt(sideC);
 
     if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
-    triType.textContent = "Please enter numbers only!";
+       alert("Please enter numbers only!");
+    }
+    if (sideA<=0 ||sideB<=0 || sideC<=0){
+      alert("Values too small to form a triangle");
     }
 
     if (a===b && b===c && c===a) {

10b8e86c4b0074eb9b1ced27e8a0bedd6679a643
luka wandanje <lukawandanje@gmail.com>
2019-03-24 20:28:45 +0300

    added new features

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,32 @@
 
 
-function triangle (sideA,sideB,sideC) {
-    var sideA = prompt("Enter a value for side A: ");
-    var sideB = prompt("Enter a value for side B: ");
-    var sideC = prompt("Enter a value for side A: ");
+function triangle(){
+    var sideA = parseFloat(document.getElementById("A").value);
+    var sideB = parseFloat(document.getElementById("B").value);
+    var sideC = parseFloat(document.getElementById("C").value);
+    var triType = document.getElementById("triType");
 
-    var a = parseInt(sideA);
-    var b = parseInt(sideB);
-    var c = parseInt(sideC);
-
-    if (sideA<=0 || sideB<=0 || sideC<=0) {
-      alert("Values too small to form a triangle")
+    if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+        triType.textContent = "Please enter numbers only!";
+    }
+    else if(sideA <=0 ||  sideB <= 0 || sideC <= 0){
+        triType.textContent = "values too small to form triangle"
+    }
+    else if ((sideA===sideB && sideB===sideC)){
+        var equilateral = "equilateral";
+        triType.textContent = "Triangle : " + equilateral;
+    }
+    else if (sideA+sideB <= sideC || sideA+sideC <= sideB || sideB+sideC <= sideA ) {
+        triType.textContent = "Not a Triangle! the sum of any two sides of the triangle must be greater than the third side.Try again";
+    }
+    else if (sideA===sideB && sideA!==sideC && sideB!==sideC || sideB===sideC
+        && sideB!==sideA && sideC!==sideA || sideA===sideC && sideA!==sideB && sideB!==sideC) {
+        var isosceles = "isosceles";
+        triType.textContent = "Triangle : " + isosceles;
+    }
+    else  {
+        var scalene = "scalene";
+        triType.textContent = "Triangle : " + scalene;
     }
 
-    if (a===b && b===c && c===a) {
-        alert("Equilateral");
-    }
-    else if (a===b && a!==c && b!==c || b===c && b!==a && c!==a || a===c && a!==b && b!==c) {
-        alert("isosceles");
-    }
-    else if(a!==b && b!==c && c!==a)  {
-        alert("scalene");
-    }
-    else{
-        alert("Not a Number");
-    }
 }
-triangle();
-
-
-if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
-    triType.textContent = "Please enter numbers only!";
-}
-(sideA===null || sideB===null || sideC===null)
